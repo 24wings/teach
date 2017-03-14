73e08dcb9e1d2c244c02d313a3f0a0f01bd2025d
unknown <>
2017-03-14 23:23:14 +0800

    express 博客系统

---
+++ gulpFile.js
@@ -0,0 +1,31 @@
+var gulp = require("gulp");
+var tsc = require("gulp-typescript-compiler");
+var ts = require('gulp-typescript');
+var nodemon = require("gulp-nodemon");
+var tsProject = ts.createProject('tsconfig.json');
+
+gulp.task("default", ["compile", "watch", "nodemon"]);
+
+
+gulp.task("watch", function () {
+    return gulp.watch("src/**/*.*", ["compile"]);
+
+});
+
+gulp.task("compile", function () {
+    return gulp.src('src/**/*.ts')
+        .pipe(tsProject())
+        .pipe(gulp.dest('build'));
+
+});
+
+gulp.task("nodemon", function () {
+    nodemon({
+        script: "build/www",
+        exec: ' set DEBUG=*,-not_this &node --debug ',
+        env: {
+            'NODE_ENV': 'production'
+        }
+
+    });
+});
